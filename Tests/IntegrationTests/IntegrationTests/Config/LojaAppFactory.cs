0e69fda5d2ede788796ce1715b8c9cff5726607e
Glauco Diogo <glaucodiogoalves@gmail.com>
2020-03-09 03:57:57 -0300

    adicionando fixture

--- Tests/IntegrationTests/IntegrationTests/Config/LojaAppFactory.cs
+++ Tests/IntegrationTests/IntegrationTests/Config/LojaAppFactory.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace IntegrationTests.Config
 {
-    public class LojaAppFactory
+    /// <summary>
+    /// A classe loja Factory está herdando da classe WebApplicationFactory, esperando que seja passado classe TStartup
+    /// where TStartup seja uma classe, isto é classe genérica.
+    /// </summary>
+    /// <typeparam name="TStartup"></typeparam>
+    public class LojaAppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseStartup<TStartup>();
+            //O contexto de teste irá buscar as variáveis de ambiente configurado no startup
+            builder.UseEnvironment("Testing");
+        }
     }
 }
